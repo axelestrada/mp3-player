503aa42ee3b1b4f572714ed663b715258bfda222
Axel Estrada <axele1524@gmail.com>
2020-12-14 21:17:42 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -523,7 +523,7 @@ let canvas,
 
 const initMP3Player = () => {
     context = new AudioContext();
-    contet.resume();
+    context.resume();
     analyser = context.createAnalyser();
 
     canvas = document.getElementById("analyser-render");
